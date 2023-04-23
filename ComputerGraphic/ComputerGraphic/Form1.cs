5b3c7416eb4137334242b7075c8333f4264515d5
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-04-23 15:33:16 -0300

    Inicio projeto

---
+++ ComputerGraphic/ComputerGraphic/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComputerGraphic.Models;
+
+namespace ComputerGraphic
+{
+    public partial class ComputerGraphic : Form
+    {
+
+        Objeto3d objeto3D;
+
+        public ComputerGraphic()
+        {
+            InitializeComponent();
+            objeto3D = new Objeto3d();
+        }
+
+        private void abrirObjeto3D_Click(object sender, EventArgs e)
+        {
+            String line;
+            try
+            {
+
+                using (var ofd = new OpenFileDialog())
+                {
+                    //ofd.Filter = "txt fil"
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        using (StreamReader arquivo = new StreamReader(ofd.FileName))
+                        {
+                            double x, y, z;
+                            string linha;
+                            string[] dados;
+                            string[] dadosFace;
+                            while ((linha = arquivo.ReadLine()) != null)
+                            {
+                                dados = linha.Split(' ');
+
+                                switch (dados[0])
+                                {
+                                    case "v":
+                                        // Pode dar Erro na converção
+                                        x = Convert.ToDouble(dados[1]);
+                                        y = Convert.ToDouble(dados[1]);
+                                        z = 0.0;
+                                        objeto3D.ListaVerticesOriginais
+                                            .Add(new Vertice(x, y, z));
+                                        objeto3D.ListaVerticesAtuais
+                                            .Add(new Vertice(x, y, z));
+                                        break;
+
+                                    case "f":
+                                        List<int> posVertices = new List<int>();
+                                        for (int i = 1; i < dados.Length; i++)
+                                        {
+                                            dadosFace = dados[i].Split('/');
+                                            // Coloca "-1", porque, as posições no arquivo iniciam de 1,
+                                            // mas, na lista inicia de 0
+                                            posVertices.Add(Convert.ToInt32(dadosFace[0]) - 1);
+                                        }
+
+                                        objeto3D.ListaFaces.Add(posVertices);
+                                        break;
+                                    default:
+                                        Console.WriteLine("Nenhuma das Opções");
+                                        break;
+                                }
+
+
+
+                            }
+
+
+
+
+                        }
+                    }
+
+
+
+
+                }
+
+
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Sample.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Exception: " + error.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
---
+++ ComputerGraphic/ComputerGraphic/Models/Objeto3d.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphic.Models
+{
+    public class Objeto3d
+    {
+        public double[,] MatrizAcumulada { get; set; }
+        public List<Vertice> ListaVerticesOriginais { get; set; }
+        public List<Vertice> ListaVerticesAtuais { get; set; }
+        // ListaFaces tem uma lista interna que guarda a posição da lista de vertices
+        public List<List<int>> ListaFaces { get; set; }
+        // Vai ter {x,y,z} só que um para cada face, vai ser do tamanho
+        // da lista de faces
+        public List<Vertice> ListaNormaisFaces { get; set; }
+        // Vai ter {x,y,z} só que um para cada vertice, vai ser do tamanho
+        // da lista de vertices, nesse caso aqui é média das normais da faces
+        // que incidem nele
+        public List<Vertice> ListaNormaisVertices { get; set; }
+
+        public Objeto3d()
+        {
+            InicializaMatriz();
+            ListaVerticesOriginais = new List<Vertice>();
+            ListaVerticesAtuais = new List<Vertice>();
+            ListaFaces = new List<List<int>>();
+            ListaNormaisFaces = new List<Vertice>();
+            ListaNormaisVertices = new List<Vertice>();
+        }
+
+
+        private void InicializaMatriz()
+        {
+            MatrizAcumulada = new double[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                MatrizAcumulada[i, i] = 1;
+            }
+        }
+
+
+
+
+
+
+    }
+}
